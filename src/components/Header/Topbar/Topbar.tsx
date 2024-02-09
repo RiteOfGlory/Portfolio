4679bee25d48bbb158a828953e97fb0068945608
Eli Fischl <vio@ocf.berkeley.edu>
2024-02-09 11:21:00 -0800

    Added topbar divider
    - Also removed meme title

--- src/components/Header/Topbar/Topbar.tsx
+++ src/components/Header/Topbar/Topbar.tsx
@@ -1,14 +1,19 @@
 import './Topbar.scss';
+import topbarDivider from './topbar-divider.svg';
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="topbar-text">
-                <div className="topbar-title">Theon Bigcockinson</div>
-                <div className="topbar-motto">Are ya winnin' son?</div>
+                {/*<div className="topbar-title">Theon Bigcockinson</div>*/}
+                {/*<div className="topbar-motto">Are ya winnin' son?</div>*/}
+                <div className="topbar-title">Victor Alves</div>
+                <div className="topbar-divider">
+                    <img src={topbarDivider} alt="divider"/>
+                </div>
             </div>
             <div className="navbar">
-                <a href="/">Home</a>
+            <a href="/">Home</a>
                 <a href="/">Artbook</a>
                 <a href="/">About Me</a>
             </div>
