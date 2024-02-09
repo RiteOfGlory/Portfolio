f78bc2131600de5f4a2551892fcfe3c20a905079
Eli Fischl <vio@ocf.berkeley.edu>
2024-02-09 11:23:11 -0800

    Added Dropdown
    - Dropdown with 3 refs
    - Added onClick Handler for clicking on the artbook div to open and anywhere else or the artbook again to close
    - TODO: style the dropdown better

--- src/components/Header/Topbar/Topbar.tsx
+++ src/components/Header/Topbar/Topbar.tsx
@@ -1,7 +1,37 @@
 import './Topbar.scss';
 import topbarDivider from './topbar-divider.svg';
+import {useState, useRef, useEffect} from "react";
+
+function ArtbookDropdown() {
+    return (
+        <div className="artbook-dropdown">
+            <a href="/public">Concept Art</a>
+            <a href="/public">Webdesign</a>
+            <a href="/public">Sketches</a>
+        </div>
+    );
+}
 
 export default function Topbar() {
+    const [isArtbookDropdownOpen, setIsArtbookDropdownOpen] = useState<boolean>(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    // Create a handler to click anywhere else on the page to close the dropdown
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setIsArtbookDropdownOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [dropdownRef]);
+
+
+
     return (
         <div className="topbar">
             <div className="topbar-text">
@@ -13,8 +43,15 @@ export default function Topbar() {
                 </div>
             </div>
             <div className="navbar">
-            <a href="/">Home</a>
-                <a href="/">Artbook</a>
+                <a href="/">Home</a>
+                <div
+                    className="artbook"
+                    onClick={ () => setIsArtbookDropdownOpen(!isArtbookDropdownOpen)}
+                    ref={dropdownRef}
+                >
+                    <div className={`artbook-text ${isArtbookDropdownOpen ? 'active' : ''}`}>Artbook</div>
+                    {isArtbookDropdownOpen && <ArtbookDropdown />}
+                </div>
                 <a href="/">About Me</a>
             </div>
         </div>
