19e8b35b5a81c4c1343e786afd605ba2e7cfb694
Eli Fischl <vio@ocf.berkeley.edu>
2024-02-09 18:08:34 -0800

    Created Footer
    - Empty Footer
    - Added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <div className="App">
         <Header />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.scss';
+
+export default function Footer() {
+    return (
+        <footer className="footer-wrapper">
+            <div className="footer-content">
+            </div>
+        </footer>
+    )
+}
