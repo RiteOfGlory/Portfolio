a1a89e9276b1df86179cf221da2c5ad3ab4d7537
Eli Fischl <vio@ocf.berkeley.edu>
2024-02-12 23:13:08 -0800

    Created Home Component
    - Added Home Component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MainSignature from "./assets/MainSignature_GoldWords.webp";
+import './Home.scss';
+
+export default function Home() {
+
+    return (
+        <div className="home-wrapper">
+            <div className="home-signature">
+                <img src={MainSignature} alt="Rite Of Glory"/>
+            </div>
+        </div>
+    )
+}
