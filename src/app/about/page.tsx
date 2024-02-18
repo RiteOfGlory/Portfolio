e334499829ae7a30030bbc88bd4c1cb2f064a44b
Eli Fischl <vio@ocf.berkeley.edu>
2024-02-17 21:29:41 -0800

    Added all pages
    - All pages are "Currently in Development"

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="flex justify-center my-4">
+            <div className="text-4xl font-semibold text-slate-300">Currently in Development</div>
+        </div>
+    )
+}
---
+++ src/app/art/webdesign/page.tsx
@@ -0,0 +1,7 @@
+export default function WebDesign() {
+    return (
+        <div className="flex justify-center my-4">
+            <div className="text-4xl font-semibold text-slate-300">Currently in Development</div>
+        </div>
+    )
+}
